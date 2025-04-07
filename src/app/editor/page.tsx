dcc10caab21ef4dafa3c08f7e98f739755df67b1
SangHwi <brilyent72@gmail.com>
2025-04-07 17:51:48 +0900

    feat: 코드 편집기 기본 코드 추가

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import CodeEditor from "@/components/editor/CodeEditor";
+
 export default function EditorPage() {
 
 
@@ -7,6 +9,7 @@ export default function EditorPage() {
 
     return (
         <main className="min-h-screen bg-[#1E1E1E]">
+            <CodeEditor />
 
         </main>
     );
--- src/components/editor/CodeEditor.tsx
+++ src/components/editor/CodeEditor.tsx
@@ -1,1 +1,18 @@
 "use client";
+
+import React from "react";
+import { Editor } from "@monaco-editor/react";
+
+const CodeEditor = () => {
+  return (
+    <div className="h-[500px]">
+      <Editor
+        height="100%" 
+        defaultLanguage="javascript" 
+        defaultValue="// 코드 입력" 
+        theme="vs-dark" />
+    </div>
+  );
+};
+
+export default CodeEditor;
