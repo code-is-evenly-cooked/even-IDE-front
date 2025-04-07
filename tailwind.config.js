36635efe46e75ff20c62ff79686a96d4fe2e0dfb
seongmin <min@seongminui-MacBookPro.local>
2025-04-08 00:02:12 +0900

    fix: tailwindCSS 버전 변경 및 재세팅

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			colors: {
+				violet300: "#B29BF8",
+				violet600: "#744CEB",
+				green500: "#00A881",
+				orange500: "#F57D14",
+				red500: "#F5535E",
+				blue500: "#5094FA",
+				tonedown: "#3E3655",
+
+				gray900: "#1E1E1E",
+				gray800: "#2A2A2A",
+				gray700: "#262626",
+				gray500: "#3F3F46",
+				gray200: "#A9ABB8",
+			},
+		},
+	},
+	plugins: [],
+};
