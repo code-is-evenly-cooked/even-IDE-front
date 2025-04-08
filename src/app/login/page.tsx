8ee887ed4c6420773da52cd1ea69d5caa3ce6235
seongmin <min@seongminui-MacBookPro.local>
2025-04-08 10:51:10 +0900

    feat: 로그인 페이지 추가

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import LoginForm from "@/components/auth/LoginForm";
+import TestInput from "@/components/auth/TestInput";
+import React from "react";
+
+const LoginPage = () => {
+	return (
+		<div className="flex justify-center items-center">
+			<LoginForm />
+		</div>
+	);
+};
+
+export default LoginPage;
