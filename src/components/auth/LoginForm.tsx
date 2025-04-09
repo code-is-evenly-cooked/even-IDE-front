e3dd413b1461bfc6b33c28bff923780725981f25
seongmin <min@seongminui-MacBookPro.local>
2025-04-09 14:21:51 +0900

    feat: 사용법 확인용 코드 추가

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -2,12 +2,23 @@
 
 import React from "react";
 import TextInput from "../common/Input/TextInput";
+import BaseButton from "../common/Button/BaseButton";
+import SocialLoginButton from "../common/Button/SocialLoginButton";
+import { EyeIcon } from "../common/Icons";
 
 const LoginForm = () => {
 	return (
 		<div className="flex flex-col w-full max-w-[40rem] gap-4">
 			<TextInput placeholder="test" size="xl" />
 			<TextInput placeholder="test2" size="xl" styleState="invalid" />
+			<BaseButton type="button" size="xl" textColor="violet300" style="outline">
+				회원가입
+			</BaseButton>
+			<BaseButton type="button" size="xl" leftIcon={<EyeIcon />}>
+				회원가입
+			</BaseButton>
+			<SocialLoginButton provider="google" />
+			<SocialLoginButton provider="kakao" />
 		</div>
 	);
 };
