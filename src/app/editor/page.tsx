0af5b9b5e24cc77679e6b4cdd336a77c044a2b22
SangHwi <brilyent72@gmail.com>
2025-04-09 12:36:05 +0900

    feat: 실행 버튼 컴포넌트 구현, editor 페이지와 연결

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,20 +1,27 @@
 "use client";
 
 import CodeEditor from "@/components/editor/CodeEditor";
-import TerminalView from '@/components/editor/Terminal';
+import TerminalView from "@/components/editor/Terminal";
+import RunButton from "@/components/editor/RunButton";
+
+import { useRef } from "react";
+import type { Terminal as XtermType } from "xterm";
 
 export default function EditorPage() {
+  const terminalRef = useRef<XtermType | null>(null);
+  const handleRun = (code: string) => {
+    if (terminalRef.current) {
+      terminalRef.current.write(`\r\n실행한 코드:\r\n${code}\r\n`);
+    }
+  };
 
-
-
-
-    return (
-        <main className="min-h-screen flex flex-col bg-[#262626]">
-            <div className="flex-1">
-            <CodeEditor />
-            </div>
-            <TerminalView />
-
-        </main>
-    );
+  return (
+    <main className="min-h-screen flex flex-col bg-[#262626]">
+        <RunButton onRun={handleRun} />
+      <div className="flex-1">
+        <CodeEditor />
+      </div>
+      <TerminalView terminalRef={terminalRef} />
+    </main>
+  );
 }
