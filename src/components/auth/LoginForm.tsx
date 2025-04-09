ee041cf092337dc0f4afc3f143fef0414ab28490
seongmin <min@seongminui-MacBookPro.local>
2025-04-09 17:44:45 +0900

    feat: 로그인 UI수정

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -4,19 +4,20 @@ import React from "react";
 import TextInput from "../common/Input/TextInput";
 import BaseButton from "../common/Button/BaseButton";
 import SocialLoginButton from "../common/Button/SocialLoginButton";
-import { EyeIcon } from "../common/Icons";
+import PasswordInput from "../common/Input/PasswordInput";
 
 const LoginForm = () => {
 	return (
 		<div className="flex flex-col w-full max-w-[40rem] gap-4">
-			<TextInput placeholder="test" size="xl" />
-			<TextInput placeholder="test2" size="xl" styleState="invalid" />
+			<TextInput placeholder="이메일을 입력하세요" size="xl" />
+			<PasswordInput placeholder="비밀먼호를 입력하세요" size="xl" />
+			<BaseButton type="button" size="xl">
+				로그인
+			</BaseButton>
 			<BaseButton type="button" size="xl" textColor="violet300" style="outline">
 				회원가입
 			</BaseButton>
-			<BaseButton type="button" size="xl" leftIcon={<EyeIcon />}>
-				회원가입
-			</BaseButton>
+
 			<SocialLoginButton provider="google" />
 			<SocialLoginButton provider="kakao" />
 		</div>
