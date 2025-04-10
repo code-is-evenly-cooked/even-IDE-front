252fdbf077b2ffe2c47b95725eff8a5b5e1c7794
SangHwi <brilyent72@gmail.com>
2025-04-10 14:47:45 +0900

    feat: 에디터 페이지 기존 헤더 hidden 처리 조건 추가
    /edtior 나 /editor/* 주소에서 Header.tsx 가 hidden 처리 됨

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,17 +1,27 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Header = () => {
-	return (
-		<header className="w-full h-[3rem] flex justify-between items-center p-4">
-			<div></div>
-			<div className="flex">
-				<Link href="/login">
-					<span className="text-link">로그인</span>
-				</Link>
-			</div>
-		</header>
-	);
+  const pathname = usePathname();
+  const isEditorPage = pathname.startsWith("/editor");
+
+  return (
+    <header
+      className={`w-full h-[3rem] flex justify-between items-center p-4 ${
+        isEditorPage ? "hidden" : ""
+      }`}
+    >
+      <div></div>
+      <div className="flex">
+        <Link href="/login">
+          <span className="text-link">로그인</span>
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
