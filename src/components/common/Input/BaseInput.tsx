1a8902af347877be10b812cc2e2398feaa87884b
seongmin <min@seongminui-MacBookPro.local>
2025-04-10 14:23:10 +0900

    design: 버튼 폰트, 오류 메세지 색상 변경

--- src/components/common/Input/BaseInput.tsx
+++ src/components/common/Input/BaseInput.tsx
@@ -94,8 +94,8 @@ const BaseInput = memo(
 
 					{(error || helper) && (
 						<p
-							className={`mt-1 text-sm ${
-								error ? "text-violet600" : "text-white"
+							className={`mt-2 mx-2 text-sm ${
+								error ? "text-red500" : "text-white"
 							}`}
 						>
 							{error || helper}
