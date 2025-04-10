433ce2cc095644f53a1e423af0c10faa7c350bb4
seongmin <min@seongminui-MacBookPro.local>
2025-04-10 14:23:26 +0900

    feat: 회원가입 UI 생성

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,12 @@
+import SignupForm from "@/components/auth/Signup/SignupForm";
 import React from "react";
 
 const SignupPage = () => {
-	return <div></div>;
+	return (
+		<div className="flex justify-center items-center">
+			<SignupForm />
+		</div>
+	);
 };
 
 export default SignupPage;
---
+++ src/components/auth/Signup/SignupForm.tsx
@@ -0,0 +1,39 @@
+import BaseButton from "@/components/common/Button/BaseButton";
+import { EvenIcon } from "@/components/common/Icons";
+import PasswordInput from "@/components/common/Input/PasswordInput";
+import TextInput from "@/components/common/Input/TextInput";
+import Link from "next/link";
+import React from "react";
+
+const SignupForm = () => {
+	return (
+		<div className="w-full max-w-[30rem] border border-white mt-16">
+			<div className="flex justify-center items-center pt-8 pb-4 gap-4">
+				<EvenIcon />
+				<h1 className="text-4xl">even ide</h1>
+			</div>
+			<div className="flex flex-col gap-5 px-6 sm:px-12 py-6">
+				{/* 회원가입 */}
+				<form className="flex flex-col gap-5">
+					<div className="flex flex-col gap-2">
+						<TextInput placeholder="이메일을 입력하세요" size="xl" />
+						<PasswordInput placeholder="비밀번호를 입력하세요" size="xl" />
+						<PasswordInput placeholder="비밀번호 확인" size="xl" />
+						<TextInput placeholder="닉네임을 입력하세요" size="xl" />
+					</div>
+
+					<BaseButton type="submit" size="xl">
+						회원가입
+					</BaseButton>
+				</form>
+				<div className="flex justify-center text-sm text-gray200 font-light">
+					<Link href="/login" className="underline hover:opacity-80 transition">
+						로그인으로 돌아가기
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SignupForm;
