d7adafbda5f386e504455738e3c2da906a5a594e
SangHwi <brilyent72@gmail.com>
2025-04-10 16:57:25 +0900

    feat: 코드 편집기 전용 헤더 page.tsx 추가

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import RunButton from "@/components/editor/RunButton";
+import Sidebar from "@/components/layout/Sidebar/Sidebar";
+import EditorHeader from "@/components/layout/Header/EditorHeader";
 import { useRef } from "react";
 import type { Terminal as XtermType } from "xterm";
 import dynamic from "next/dynamic";
@@ -22,12 +24,18 @@ export default function EditorPage() {
   };
 
   return (
-    <main className="min-h-screen flex flex-col bg-[#262626]">
-      <RunButton onRun={handleRun} />
-      <div className="flex-1">
-        <CodeEditor />
+    <div>
+      <EditorHeader />
+      <div className="flex h-screen w-screen overflow-hidden">
+        <Sidebar />
+        <main className="min-h-screen flex flex-col bg-[#262626]">
+          <RunButton onRun={handleRun} />
+          <div className="mt-[20px]">
+            <CodeEditor />
+          </div>
+          <TerminalView terminalRef={terminalRef} />
+        </main>
       </div>
-      <TerminalView terminalRef={terminalRef} />
-    </main>
+    </div>
   );
 }
