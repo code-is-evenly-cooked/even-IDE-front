2d6f2b52549456f5eb7e472e8c38269ee0f1798e
leehyunha <jewel2503@naver.com>
2025-04-12 03:30:36 +0900

    로그인 유도 팝업창 페이지 구현

---
+++ src/components/loginpopup/Popup.tsx
@@ -0,0 +1,35 @@
+// 로그인 유도 팝업창 페이지
+"use client";
+
+import React from "react";
+import TextInput from "@/components/common/Input/TextInput";
+import BaseButton from "@/components/common/Button/BaseButton";
+import { EvenIcon } from "@/components/common/Icons";
+import Link from "next/link";
+// useLoginPopupForm 혹은 로그인 팝업 전용 파일에서 import
+
+const Popup = () => {
+
+	return (
+		<div className="w-full max-w-[30rem] border border-white mt-16">
+			<div className="flex justify-center items-center pt-8 pb-4 gap-4">
+				<EvenIcon />
+				<h1 className="text-4xl">even ide</h1>
+			</div>
+
+			<div className="flex flex-col gap-5 px-6 sm:px-12 py-6">
+				<form  className="flex flex-col  gap-5">
+					<h1 className="text-xl text-white text-center">
+						로그인하여 나만의 개발 환경을 시작하세요.
+					</h1>
+
+					<BaseButton type="submit" size="xl">
+						로그인 하러가기
+					</BaseButton>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default Popup;
