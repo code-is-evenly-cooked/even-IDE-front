578a5370cb8641e39237fa2fc6ddcc3a45a82a04
SangHwi <brilyent72@gmail.com>
2025-04-13 01:53:28 +0900

    feat: 이븐 로고 추가

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -1,12 +1,16 @@
 import FileExplorer from "@/components/editor/FileExplorer";
 import { Plus, Undo2, Trash2, FolderPlus } from "lucide-react";
+import { EvenIcon } from "../../common/Icons";
 
 export default function Sidebar() {
   return (
     <aside className="w-[240px] min-w-[240px] h-screen bg-gray700 text-white flex flex-col">
       {/* 상단 로고 */}
-      <div className="px-4 py-3 text-lg font-bold border-b border-gray-700">
-        even ide 로고
+      <div className="flex px-5 py-3 content-center text-lg font-bold border-b border-gray-700 bg-tonedown">
+        <div>
+          <EvenIcon />
+        </div>
+        <h1 className="text-3xl font-light ml-3">even ide</h1>
       </div>
 
       {/* 버튼 영역 */}
