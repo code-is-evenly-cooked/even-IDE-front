3ffc92a470212ea0a54f58010b118f6755c076ae
SangHwi <brilyent72@gmail.com>
2025-04-14 14:42:13 +0900

    feat: 사이드 바 Icon 파일 추가와 Icons 컴포넌트 등록

--- src/components/common/Icons/index.tsx
+++ src/components/common/Icons/index.tsx
@@ -67,3 +67,55 @@ export const EvenIcon = ({ className }: IconProps) => {
 		/>
 	);
 };
+
+export const FileNewIcon = ({ className }: IconProps) => {
+	return (
+		<Image
+			src="/icons/fileNew.svg"
+			alt="새 파일"
+			width={40}
+			height={40}
+			className={className}
+			priority
+		/>
+	);
+};
+
+export const FolderNewIcon = ({ className }: IconProps) => {
+	return (
+		<Image
+			src="/icons/folderNew.svg"
+			alt="새 프로젝트"
+			width={40}
+			height={40}
+			className={className}
+			priority
+		/>
+	);
+};
+
+export const BackIcon = ({ className }: IconProps) => {
+	return (
+		<Image
+			src="/icons/back.svg"
+			alt="되돌리기"
+			width={40}
+			height={40}
+			className={className}
+			priority
+		/>
+	);
+};
+
+export const CloseIcon = ({ className }: IconProps) => {
+	return (
+		<Image
+			src="/icons/close2.svg"
+			alt="닫기"
+			width={40}
+			height={40}
+			className={className}
+			priority
+		/>
+	);
+};
