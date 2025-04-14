3c91732ace6172610aec392dcff7cf6eb49138c5
leehyunha <jewel2503@naver.com>
2025-04-14 22:47:53 +0900

    비로그인 팝업창 스타일링
    (헤드 문구, 로그인 유도 문구, 로그인 하러가기 버튼) 크기 조정

--- src/components/loginpopup/Popup.tsx
+++ src/components/loginpopup/Popup.tsx
@@ -5,24 +5,22 @@ import React from "react";
 import TextInput from "@/components/common/Input/TextInput";
 import BaseButton from "@/components/common/Button/BaseButton";
 import { EvenIcon } from "@/components/common/Icons";
+import page from "@/app/hyunhatest/page";
 import Link from "next/link";
 // useLoginPopupForm 혹은 로그인 팝업 전용 파일에서 import
 
 const Popup = () => {
-
 	return (
-		<div className="w-full max-w-[30rem] border border-white mt-16">
+		<div className="w-full max-w-[33rem] border border-black mt-26 bg-[#2b2b2b] p-6 rounded-xl">
 			<div className="flex justify-center items-center pt-8 pb-4 gap-4">
 				<EvenIcon />
-				<h1 className="text-4xl">even ide</h1>
+				<h1 className="text-3xl">even ide</h1>
 			</div>
-
-			<div className="flex flex-col gap-5 px-6 sm:px-12 py-6">
-				<form  className="flex flex-col  gap-5">
+			<div className="flex flex-col gap-5 px-6 sm:px-12 py-10">
+				<form className="flex flex-col gap-20">
 					<h1 className="text-xl text-white text-center">
 						로그인하여 나만의 개발 환경을 시작하세요.
 					</h1>
-
 					<BaseButton type="submit" size="xl">
 						로그인 하러가기
 					</BaseButton>
