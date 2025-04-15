9e5841afc5871d82430e0f56654ebafeecc7b0d5
seongmin <min@seongminui-MacBookPro.local>
2025-04-15 09:12:29 +0900

    remove: 디버깅용 로그 제거

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -5,7 +5,6 @@ import { handleApiResponse } from "@/lib/api";
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function POST(req: NextRequest) {
-	console.log("API_BASE_URL", API_BASE_URL);
 	try {
 		const body = await req.json();
 		const response = await fetch(`${API_BASE_URL}/auth/signup`, {
