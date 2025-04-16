a2942da63ee0f0dbe1fd248b69a1173c355d5972
경희 <101131146+jdaily00@users.noreply.github.com>
2025-04-16 10:42:20 +0900

    Update LoginForm.tsx
    LoginForm 파일 메인 브랜치 코드로 수정

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,24 +1,96 @@
 "use client";
 
 import React from "react";
-import TextInput from "../common/Input/TextInput";
-import BaseButton from "../common/Button/BaseButton";
-import SocialLoginButton from "../common/Button/SocialLoginButton";
-import { EyeIcon } from "../common/Icons";
+import TextInput from "../../common/Input/TextInput";
+import BaseButton from "../../common/Button/BaseButton";
+import SocialLoginButton from "../../common/Button/SocialLoginButton";
+import PasswordInput from "../../common/Input/PasswordInput";
+import Link from "next/link";
+import { EvenIcon } from "../../common/Icons";
+import useLoginForm from "./useLoginForm";
 
 const LoginForm = () => {
+	const {
+		formState,
+		errors,
+		isLoading,
+		handleFormChange,
+		handleLogin,
+		handleSignup,
+		handleGoogleLogin,
+		handleKakaoLogin,
+	} = useLoginForm();
+
 	return (
-		<div className="flex flex-col w-full max-w-[40rem] gap-4">
-			<TextInput placeholder="test" size="xl" />
-			<TextInput placeholder="test2" size="xl" styleState="invalid" />
-			<BaseButton type="button" size="xl" textColor="violet300" style="outline">
-				회원가입
-			</BaseButton>
-			<BaseButton type="button" size="xl" leftIcon={<EyeIcon />}>
-				회원가입
-			</BaseButton>
-			<SocialLoginButton provider="google" />
-			<SocialLoginButton provider="kakao" />
+		<div className="w-full max-w-[30rem] border border-white mt-16">
+			<div className="flex justify-center items-center pt-8 pb-4 gap-4">
+				<EvenIcon />
+				<h1 className="text-4xl">even ide</h1>
+			</div>
+
+			<div className="flex flex-col gap-5 px-6 sm:px-12 py-6">
+				{/* 로그인 */}
+				<div className="flex flex-col gap-1.5">
+					<form onSubmit={handleLogin} className="flex flex-col gap-5">
+						<div className="flex flex-col gap-2">
+							<TextInput
+								placeholder="이메일을 입력하세요"
+								size="xl"
+								value={formState.email}
+								onChange={handleFormChange("email")}
+								styleState={errors.email ? "invalid" : "default"}
+								error={errors.email}
+							/>
+							<PasswordInput
+								placeholder="비밀번호를 입력하세요"
+								size="xl"
+								value={formState.password}
+								onChange={handleFormChange("password")}
+								styleState={errors.password ? "invalid" : "default"}
+								error={errors.password}
+							/>
+						</div>
+
+						<BaseButton type="submit" size="xl" isLoading={isLoading}>
+							로그인
+						</BaseButton>
+					</form>
+
+					{/* 회원가입 */}
+					<BaseButton
+						type="button"
+						size="xl"
+						textColor="violet300"
+						style="outline"
+						onClick={handleSignup}
+					>
+						회원가입
+					</BaseButton>
+				</div>
+
+				{/* 비밀번호 재설정 */}
+				<div className="flex justify-center text-sm text-gray200 font-light">
+					<Link
+						href="/reset-password"
+						className="underline hover:opacity-80 transition"
+					>
+						비밀번호 재설정
+					</Link>
+				</div>
+
+				{/* 또는 구분선 */}
+				<div className="flex items-center w-full text-gray200">
+					<div className="flex-1 h-px bg-gray200" />
+					<span className="px-4 text-gray200 text-sm leading-none">또는</span>
+					<div className="flex-1 h-px bg-gray200" />
+				</div>
+
+				{/* 소셜 로그인 */}
+				<div className="flex flex-col gap-2">
+					<SocialLoginButton provider="google" onClick={handleGoogleLogin} />
+					<SocialLoginButton provider="kakao" onClick={handleKakaoLogin} />
+				</div>
+			</div>
 		</div>
 	);
 };
