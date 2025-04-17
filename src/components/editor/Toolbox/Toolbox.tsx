83c4653a7b80a785b5bd9c4f00aff8501d0bb84b
seongmin <min@seongminui-MacBookPro.local>
2025-04-17 13:36:16 +0900

    feat: toolbox내 아이템 추가

--- src/components/editor/Toolbox/Toolbox.tsx
+++ src/components/editor/Toolbox/Toolbox.tsx
@@ -1,17 +1,39 @@
-import React from "react";
-import IconButton from "../../common/Button/IconButton";
+import React, { ReactNode } from "react";
 import { MessageCircleCodeIcon } from "lucide-react";
+import IconButton from "@/components/common/Button/IconButton";
+import { PanelType } from "@/types/panel";
 
-const Toolbox = () => {
+interface ToolboxProps {
+	activePanel: PanelType | null;
+	onSelect: (PanelType: PanelType | null) => void;
+}
+
+const Toolbox = ({ onSelect, activePanel }: ToolboxProps) => {
+	const handleClick = (panel: PanelType) => {
+		onSelect(activePanel === panel ? null : panel);
+	};
+
+	const TOOLBOX_ITEMS: { panel: PanelType; icon: ReactNode; label: string }[] =
+		[
+			{
+				panel: "chat",
+				icon: <MessageCircleCodeIcon width={16} height={16} />,
+				label: "채팅",
+			},
+		];
+
 	return (
 		<aside className="flex flex-col w-9 mx-2 bg-gray900">
-			<IconButton
-				icon={<MessageCircleCodeIcon width={16} height={16} />}
-				label="채팅 실행"
-				color="gray700"
-				isActive={true}
-				onClick={() => {}}
-			/>
+			{TOOLBOX_ITEMS.map(({ panel, icon, label }) => (
+				<IconButton
+					key={panel}
+					icon={icon}
+					label={activePanel === panel ? `${label} 닫기` : `${label} 열기`}
+					color="gray700"
+					isActive={activePanel === panel}
+					onClick={() => handleClick(panel)}
+				/>
+			))}
 		</aside>
 	);
 };
