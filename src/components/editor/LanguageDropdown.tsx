f75109db85d5aefc7486040a19f434a6c2b90d92
SangHwi <brilyent72@gmail.com>
2025-04-18 14:55:49 +0900

    feat: 언어 드롭 다운 기능 구현

--- src/components/editor/LanguageDropdown.tsx
+++ src/components/editor/LanguageDropdown.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useLanguageStore } from "@/stores/useLanguageStore";
 
 const languages = ["Java", "Javascript", "Python"];
@@ -10,6 +10,15 @@ export default function LanguageDropdown() {
   const [open, setOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+            setOpen(false);
+        }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () =>  document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
 
   return (
     <div ref={dropdownRef} className="flex border rounded pl-4 pr-4 py-1.5">
@@ -27,7 +36,10 @@ export default function LanguageDropdown() {
                 setLanguage(lang);
                 setOpen(false);
             }}
-            ></li>
+            className={`px-4 py-2 hover:bg-gray500 cursor-pointer ${lang === language ? "bg-gray700 font-semibold" : ""}`}
+            >
+                {lang}
+            </li>
           ))}
         </ul>
       )}
