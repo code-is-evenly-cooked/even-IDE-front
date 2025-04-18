9d0ce423f66151d3f07a65af6da88f08b2ce71fc
jdaily00 <kyeonghee_@naver.com>
2025-04-18 11:28:24 +0900

    feat: 구글 소셜 로그인 성공 시 자동으로 코드편집기 페이지로 리디렉션

--- src/components/auth/LoginForm/useLoginForm.ts
+++ src/components/auth/LoginForm/useLoginForm.ts
@@ -1,86 +1,89 @@
 "use client";
 
-import { saveAuthCookie } from "@/lib/cookie";
-import { userLogin } from "@/service/auth";
-import { useAuthStore } from "@/stores/useAuthStore";
-import { validateEmail, validatePassword } from "@/utils/validate";
-import { useRouter } from "next/navigation";
+import {saveAuthCookie} from "@/lib/cookie";
+import {userLogin} from "@/service/auth";
+import {useAuthStore} from "@/stores/useAuthStore";
+import {validateEmail, validatePassword} from "@/utils/validate";
+import {useRouter} from "next/navigation";
 import {ChangeEvent, FormEvent, useCallback, useEffect, useState} from "react";
-import { signIn, useSession } from "next-auth/react";
+import {signIn, useSession} from "next-auth/react";
 
 type FormType = "email" | "password";
 
 interface LoginFormErrors {
-	email?: string;
-	password?: string;
+    email?: string;
+    password?: string;
 }
 
 const useLoginForm = () => {
-	const router = useRouter();
-	const {data: session} = useSession();
-    	const setAccessToken = useAuthStore((state) => state.setAccessToken);
-	
-	const [formState, setFormState] = useState({
-		email: "",
-		password: "",
-	});
-	const [errors, setErrors] = useState<LoginFormErrors>({});
-	const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
+    const {data: session} = useSession();
+    const setAccessToken = useAuthStore((state) => state.setAccessToken);
 
-	const validateForm = useCallback(() => {
-		const newErrors: LoginFormErrors = {};
+    const [formState, setFormState] = useState({
+        email: "",
+        password: "",
+    });
+    const [errors, setErrors] = useState<LoginFormErrors>({});
+    const [isLoading, setIsLoading] = useState(false);
 
-		const emailError = validateEmail(formState.email);
-		if (emailError) newErrors.email = emailError;
+    const validateForm = useCallback(() => {
+        const newErrors: LoginFormErrors = {};
 
-		const passwordError = validatePassword(formState.password);
-		if (passwordError) newErrors.password = passwordError;
+        const emailError = validateEmail(formState.email);
+        if (emailError) newErrors.email = emailError;
 
-		setErrors(newErrors);
-		return Object.keys(newErrors).length === 0;
-	}, [formState.email, formState.password]);
+        const passwordError = validatePassword(formState.password);
+        if (passwordError) newErrors.password = passwordError;
 
-	const handleFormChange = useCallback(
-		(key: FormType) => (e: ChangeEvent<HTMLInputElement>) => {
-			const { value } = e.target;
-			setFormState((prev) => ({ ...prev, [key]: value }));
-			setErrors((prev) => ({ ...prev, [key]: "" }));
-		},
-		[]
-	);
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    }, [formState.email, formState.password]);
 
-	const handleLogin = async (e: FormEvent) => {
-		e.preventDefault();
+    const handleFormChange = useCallback(
+        (key: FormType) => (e: ChangeEvent<HTMLInputElement>) => {
+            const {value} = e.target;
+            setFormState((prev) => ({...prev, [key]: value}));
+            setErrors((prev) => ({...prev, [key]: ""}));
+        },
+        []
+    );
 
-		if (!validateForm()) return;
+    const handleLogin = async (e: FormEvent) => {
+        e.preventDefault();
 
-		setIsLoading(true);
-		try {
-			const response = await userLogin({
-				email: formState.email,
-				password: formState.password,
-			});
-			saveAuthCookie(response.accessToken);
-			useAuthStore.getState().setAuth(response.accessToken, "local");
-			router.replace("/editor");
-		} catch (err) {
-			if (err instanceof Error) {
-				alert(err.message);
-			} else {
-				alert("알 수 없는 오류가 발생했습니다.");
-			}
-		} finally {
-			setIsLoading(false);
-		}
-	};
+        if (!validateForm()) return;
 
-	const handleSignup = () => {
-		router.push("/signup");
-	};
-
-	const handleGoogleLogin = async () => {
+        setIsLoading(true);
         try {
-            const res = await signIn("google", {redirect: false});
+            const response = await userLogin({
+                email: formState.email,
+                password: formState.password,
+            });
+            saveAuthCookie(response.accessToken);
+            useAuthStore.getState().setAuth(response.accessToken, "local");
+            router.replace("/editor");
+        } catch (err) {
+            if (err instanceof Error) {
+                alert(err.message);
+            } else {
+                alert("알 수 없는 오류가 발생했습니다.");
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const handleSignup = () => {
+        router.push("/signup");
+    };
+
+    const handleGoogleLogin = async () => {
+        try {
+            const res = await signIn("google", {
+                redirect: true,
+                callbackUrl: "/editor",
+            });
 
             if (res?.error) {
                 console.error("구글 로그인 실패", res.error);
@@ -92,9 +95,9 @@ const useLoginForm = () => {
         console.log("google Login");
     };
 
-	 const handleKakaoLogin = async () => {
+    const handleKakaoLogin = async () => {
         try {
-            const res = await signIn("kakao", { redirect: false });
+            const res = await signIn("kakao", {redirect: false});
 
             if (res?.error) {
                 console.error("카카오 로그인 실패", res.error);
@@ -144,16 +147,16 @@ const useLoginForm = () => {
         }
     }, [session?.accessToken]);
 
-	return {
-		formState,
-		errors,
-		isLoading,
-		handleFormChange,
-		handleLogin,
-		handleSignup,
-		handleGoogleLogin,
-		handleKakaoLogin,
-	};
+    return {
+        formState,
+        errors,
+        isLoading,
+        handleFormChange,
+        handleLogin,
+        handleSignup,
+        handleGoogleLogin,
+        handleKakaoLogin,
+    };
 };
 
 export default useLoginForm;
