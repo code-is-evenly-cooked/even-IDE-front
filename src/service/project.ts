611c1fb3d75e872adeba1ba5dc8bcd8ebe78ca29
SangHwi <brilyent72@gmail.com>
2025-04-21 19:52:05 +0900

    fix: api 주소 오타 수정

--- src/service/project.ts
+++ src/service/project.ts
@@ -1,25 +1,31 @@
-export const createProject = async (name: string, token: string) => {
-    const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-  
-    const res = await fetch(`${API_BASE_URL}/api/project`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({ name }),
-    });
-  
-    if (!res.ok) {
-      const error = await res.json();
-      throw new Error(error.message || "프로젝트 생성 실패");
-    }
-  
-    const data = await res.json();
-  
-    return {
-      id: data.shareUUID,
-      name: data.name,
-    };
+export const createProject = async (
+  name: string,
+  token: string,
+  ownerId: number
+) => {
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+  const res = await fetch(`${API_BASE_URL}/project`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      projectName: name,
+      ownerId: ownerId,
+    }),
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message || "프로젝트 생성 실패");
+  }
+
+  const data = await res.json();
+
+  return {
+    id: data.shareUUID,
+    name: data.name,
   };
-  
+};
