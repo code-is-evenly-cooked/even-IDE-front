78130eb72c533c86fcbe65a1f729ae6a0d91f7ad
SangHwi <brilyent72@gmail.com>
2025-04-21 19:56:15 +0900

    fix: 프로젝트 생성 api 주소 수정

--- src/service/project.ts
+++ src/service/project.ts
@@ -5,7 +5,7 @@ export const createProject = async (
 ) => {
   const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-  const res = await fetch(`${API_BASE_URL}/project`, {
+  const res = await fetch(`${API_BASE_URL}/projects`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
