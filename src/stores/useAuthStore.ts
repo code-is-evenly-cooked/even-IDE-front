0c441c005c1e254731e205111c67ea56167da525
SangHwi <brilyent72@gmail.com>
2025-04-21 19:22:22 +0900

    feat: useAuthStore 에 userId 추가

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -1,29 +1,32 @@
 import { create } from "zustand";
 
 interface AuthState {
-	isLoggedIn: boolean;
-	accessToken: string | null;
+  isLoggedIn: boolean;
+  accessToken: string | null;
+  userId: number | null;
   setAccessToken: (token: string) => void;
-	provider: "local" | "google" | "kakao" | null;
-	setAuth: (token: string, provider: AuthState["provider"]) => void;
-	clearAuth: () => void;
+  provider: "local" | "google" | "kakao" | null;
+  setAuth: (token: string, provider: AuthState["provider"]) => void;
+  clearAuth: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
-	isLoggedIn: false,
-	accessToken: null,
+  isLoggedIn: false,
+  accessToken: null,
+  userId: null,
   setAccessToken: (token) => set({ accessToken: token }),
-	provider: null,
-	setAuth: (token, provider) =>
-		set(() => ({
-			isLoggedIn: true,
-			accessToken: token,
-			provider,
-		})),
-	clearAuth: () =>
-		set(() => ({
-			isLoggedIn: false,
-			accessToken: null,
-			provider: null,
-		})),
+  provider: null,
+  setAuth: (token, provider) =>
+    set(() => ({
+      isLoggedIn: true,
+      accessToken: token,
+      provider,
+    })),
+  clearAuth: () =>
+    set(() => ({
+      isLoggedIn: false,
+      accessToken: null,
+	  userId: null,
+      provider: null,
+    })),
 }));
