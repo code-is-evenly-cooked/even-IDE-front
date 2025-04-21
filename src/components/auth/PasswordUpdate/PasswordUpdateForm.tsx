fd54ea1566b05995d261858e32a90d1852f1c5a5
leehyunha <jewel2503@naver.com>
2025-04-21 18:08:18 +0900

    통일성을 위한 코드 수정, 비밀번호 재설정 - 간격 & 높이 조정

--- src/components/auth/PasswordUpdate/PasswordUpdateForm.tsx
+++ src/components/auth/PasswordUpdate/PasswordUpdateForm.tsx
@@ -1,45 +1,54 @@
 "use client"; // 화면 마크업 //
-import React from "react";
+
+import React, { useState } from "react";
 import TextInput from "@/components/common/Input/TextInput";
-import { EvenIcon } from "@/components/common/Icons";
-import PasswordInput from "@/components/common/Input/PasswordInput";
 import BaseButton from "@/components/common/Button/BaseButton";
+import { EvenIcon } from "@/components/common/Icons";
 import Link from "next/link";
+import useResetPasswordForm from "../PasswordReset/usePasswordResetForm";
 
 const PasswordUpdateForm = () => {
-  return (
-    <div className="w-full max-w-[33rem] border border-white bg-gray900 p-6 mt-24">
-      <div className="flex justify-center items-center pt-8 pb-4 gap-4">
+  const [password, setPassword] = useState("");
+  const handlePasswordChange = () => {};
+
+  return(
+    <div className="w-full max-w-[28rem] border border-white mt-16">
+      <div className="flex justify-center items-center pt-8 pb-1 gap-4">
         <EvenIcon />
         <h1 className="text-4xl">even ide</h1>
       </div>
-      <div className="text-xl text-white text-center">
-        비밀번호 재설정
-      </div>
-      <p className="text-md text-white text-center pt-8 pb-10 gap-4">
-        새로운 비밀번호를 설정하세요. 
-      </p>
+      <div className="flex flex-col gap-5 px-6 sm:px-12 py-6">
+				<form  className="flex flex-col gap-6">
+					<h1 className="text-xl text-center mb-2">비밀번호 재설정</h1>
+					<h2 className="text-md text-center leading-loose mb-4">
+						새로운 비밀번호를 설정하세요.
+						<br />						
+					</h2>
 
-      <form className="w-full flex flex-col gap-8">
-        <PasswordInput
+          <TextInput
           placeholder="비밀번호를 입력하세요."
+          value={password}
+          onChange={handlePasswordChange}
           size="xl"
-          className="text-white placeholder-white"
-        />
-        <PasswordInput
+          />
+          <TextInput
           placeholder="비밀번호 확인"
+          value={password}
+          onChange={handlePasswordChange}          
           size="xl"
-          className="text-white placeholder-white"
-        />
-        <BaseButton type="submit" size="xl">
-          확인
-        </BaseButton>
-      </form>
+          />
 
-      <div className="flex justify-center mt-10 mb-8">
-        <Link href="/login" className="text-md text-violet-400 underline hover:opacity-80 transition"
-        >로그인으로 돌아가기
-        </Link>
+          <BaseButton type="submit" size="xl" >
+            확인
+          </BaseButton>
+          
+          <div className="flex justify-center text-sm text-violet-400 font-light mt-2 mb-4">
+            <Link href="/login" className="underline hover:opacity-80 transition"
+            >
+             로그인으로 돌아가기
+            </Link>
+          </div>
+        </form>            
       </div>
     </div>
   );
