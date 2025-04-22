b7f7485d0461954c39ff7ebd19d1a2daa400a910
seongmin <min@seongminui-MacBookPro.local>
2025-04-22 23:04:45 +0900

    feat: 채팅 내부에서 닫기 클릭 시 chat 보이지않도록 적용

--- src/components/editor/RightPanel/RightPanel.tsx
+++ src/components/editor/RightPanel/RightPanel.tsx
@@ -4,22 +4,19 @@ import { usePanelStore } from "@/stores/usePanelState";
 import ChatModal from "./Chat/ChatModal";
 import ChatPanel from "./Chat/ChatPanel";
 import AIPanel from "./AI/AIPanel";
+import { useChatStore } from "@/stores/useChatStore";
 
 const RightPanel = () => {
+	const { viewMode } = useChatStore();
 	const { activePanel } = usePanelStore();
+
+	if (!activePanel) return null;
+
 	return (
 		<>
-			{activePanel === "chat" && (
-				<>
-					<ChatPanel />
-					<ChatModal />
-				</>
-			)}
-			{activePanel === "ai" && (
-				<>
-					<AIPanel />
-				</>
-			)}
+			{activePanel === "chat" && viewMode === "panel" && <ChatPanel />}
+			{activePanel === "chat" && viewMode === "modal" && <ChatModal />}
+			{activePanel === "ai" && <AIPanel />}
 		</>
 	);
 };
