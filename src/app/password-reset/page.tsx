7f01a03a24d1d70f5aceaccec0751e297bbc9925
seongmin <min@seongminui-MacBookPro.local>
2025-04-22 21:49:30 +0900

    remove: 불필요한 주석 제거

--- src/app/password-reset/page.tsx
+++ src/app/password-reset/page.tsx
@@ -7,7 +7,6 @@ import React from "react";
 export default function PasswordResetPage() {
 	const searchParam = useSearchParams();
 	const token = searchParam.get("token");
-	console.log(token);
 
 	if (!token) return <div>잘못된 접근입니다.</div>;
 
