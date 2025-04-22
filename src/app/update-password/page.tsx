6c92ad83cbf8e093d67df72d266696014363eb5e
leehyunha <jewel2503@naver.com>
2025-04-22 11:02:05 +0900

    feat Password 변경 페이지 레이아웃 수정

--- src/app/update-password/page.tsx
+++ src/app/update-password/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function UpdatePasswordPage() {
   return (
-    <div className="min-h-screen bg-gray flex items-center justify-center px-4">
+    <div className="flex justify-center items-center">
       <PasswordUpdateForm />
     </div>
   );
