c9d8a9227edea843bba1918f4c270614031ca493
SangHwi <brilyent72@gmail.com>
2025-04-24 21:19:06 +0900

    feat: 에디터 페이지에도 코드 실행이 가능하게 수정 및 불필요한 코드 삭제

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -5,7 +5,6 @@ import Header from "@/components/layout/Header/Header";
 import { useEffect, useRef } from "react";
 import type { Terminal as XtermType } from "xterm";
 import dynamic from "next/dynamic";
-import { useLanguageStore } from "@/stores/useLanguageStore";
 import Tabbar from "@/components/editor/Tabbar";
 import Toolbox from "@/components/editor/Toolbox/Toolbox";
 import RightPanel from "@/components/editor/RightPanel/RightPanel";
@@ -44,32 +43,8 @@ export type ProjectResponse = {
 
 export default function EditorPage() {
   const terminalRef = useRef<XtermType | null>(null);
-  const language = useLanguageStore((state) => state.language);
   const { setProjects, setProjectId } = useProjectStore();
   const { setFiles } = useIdeStore();
-
-  const handleRun = (code: string) => {
-    if (!terminalRef.current) return;
-
-    terminalRef.current.clear();
-
-    if (language.toLowerCase() === "javascript") {
-      try {
-        const result = eval(code); // 코드 실행
-
-        terminalRef.current.write(`\r\n[결과] ${String(result)}\r\n`);
-      } catch (error: unknown) {
-        if (error instanceof Error) {
-          terminalRef.current.write(`\r\n[오류] ${error.message}\r\n`);
-        } else {
-          terminalRef.current.write(`\r\n[오류] 알 수 없는 오류입니다\r\n`);
-        }
-      }
-    } else {
-      // 지원되지 않는 언어에 대한 메시지
-      terminalRef.current.write(`\r\n[아직 지원되지 않는 언어입니다]\r\n`);
-    }
-  };
 
   useEffect(() => {
     const token = getAuthCookie().token;
@@ -114,7 +89,7 @@ export default function EditorPage() {
       <div className="flex h-screen">
         <Sidebar />
         <div className="flex flex-1 flex-col min-w-0">
-          <Header onRun={handleRun} />
+		<Header terminalRef={terminalRef as React.MutableRefObject<XtermType>} />
           <div className="flex min-h-0">
             <main className="min-w-0 overflow-x-hidden flex flex-1 flex-col bg-gray700">
               <div className="flex flex-col flex-1 min-w-0">
