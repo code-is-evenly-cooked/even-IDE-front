99a18c04e3a0109b79a3a439ea494593e2016c80
seongmin <min@seongminui-MacBookPro.local>
2025-04-24 12:37:53 +0900

    feat: 채팅 미연결시 오류 대신 disable로 변경

--- src/components/editor/RightPanel/Chat/ChatInput.tsx
+++ src/components/editor/RightPanel/Chat/ChatInput.tsx
@@ -2,9 +2,14 @@ import { useChat } from "@/hooks/useChat";
 import MessageInput from "./common/MessageInput";
 
 const ChatInput = () => {
-	const { sendMessage } = useChat();
+	const { sendMessage, connected } = useChat();
 
-	return <MessageInput onSubmit={(message) => sendMessage(message)} />;
+	return (
+		<MessageInput
+			onSubmit={(message) => sendMessage(message)}
+			disabled={!connected}
+		/>
+	);
 };
 
 export default ChatInput;
