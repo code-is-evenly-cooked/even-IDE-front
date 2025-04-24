c5b3e2e964985440be0d2604ac530e3f97ebe9a3
nahyun <knahyuk@gmail.com>
2025-04-24 21:55:44 +0900

    fix: 커스텀 CSS 반영

--- src/components/editor/EditLockToggle.tsx
+++ src/components/editor/EditLockToggle.tsx
@@ -38,7 +38,7 @@ export default function EditLockToggle({
     <div className="flex items-center gap-2 relative group">
       {message && (
         <div
-          className={`text-xs text-[#F5535E] bg-[#1E1E1E] px-2 py-1 rounded whitespace-nowrap transition-opacity 
+          className={`text-xs text-red500 bg-gray900 px-2 py-1 rounded whitespace-nowrap transition-opacity 
 				duration-500 ${isVisible ? "opacity-100" : "opacity-0"}`}
         >
           {message}
@@ -48,10 +48,10 @@ export default function EditLockToggle({
       <GhostIcon
         className={`w-6 h-6 transition-colors duration-500 ${
           editLocked
-            ? "text-[#3E3655]"
+            ? "text-tonedown"
             : isOwner
-            ? "text-[#262626] group-hover:text-white"
-            : "text-[#262626]"
+            ? "text-gray700 group-hover:text-white"
+            : "text-gray700"
         }`}
       />
 
@@ -61,8 +61,8 @@ export default function EditLockToggle({
           editLocked
             ? "bg-[#3E3655]"
             : isOwner
-            ? "bg-[#262626] group-hover:bg-[#A78BFA]"
-            : "bg-[#262626]"
+            ? "bg-gray800 group-hover:bg-violet300"
+            : "bg-gray800"
         }`}
       >
         <div
@@ -70,10 +70,10 @@ export default function EditLockToggle({
     w-[24px] h-[24px] rounded-full transition duration-500
     ${
       editLocked
-        ? "bg-[#262626] translate-x-0"
+        ? "bg-gray700 translate-x-0"
         : isOwner
-        ? "bg-[#1E1E1E] group-hover:bg-white translate-x-[28px]"
-        : "bg-[#1E1E1E] translate-x-[28px]"
+        ? "bg-gray900 group-hover:bg-white translate-x-[28px]"
+        : "bg-gray900 translate-x-[28px]"
     }
   `}
         />
