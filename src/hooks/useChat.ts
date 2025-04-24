c7c1e90455979842156b850312e450d3ada48a0f
seongmin <min@seongminui-MacBookPro.local>
2025-04-24 12:37:29 +0900

    feat: ChatContext 미연결시 오류 -> 경고로 변경

--- src/hooks/useChat.ts
+++ src/hooks/useChat.ts
@@ -3,6 +3,13 @@ import { useContext } from "react";
 
 export const useChat = () => {
 	const context = useContext(ChatContext);
-	if (!context) throw new Error("useChat must be used within a ChatProvider");
-	return context;
+	if (!context) {
+		return {
+			sendMessage: () => {
+				console.warn("ChatProvider가 없어서 메시지 전송이 비활성화됩니다.");
+			},
+			connected: false,
+		};
+	}
+	return { ...context, connected: true };
 };
