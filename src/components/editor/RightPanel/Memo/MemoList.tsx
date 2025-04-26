30f0eca1d355c1abb07aba60c176bd5aaf28399d
jdaily00 <kyeonghee_@naver.com>
2025-04-26 15:07:51 +0900

    feat: 메모리스트

---
+++ src/components/editor/RightPanel/Memo/MemoList.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useMemoStore } from "@/stores/useMemoStore";
+
+const MemoList = () => {
+    const {memos} = useMemoStore();
+
+    return (
+        <div className="flex flex-col gap-4 text-white text-sm">
+            {memos.map((memo) => (
+                <div key={memo.id} className="bg-gray700 p-2 rounded">
+                    <div className="text-xs text-gray400">라인 {memo.line_number}</div>
+                    <div className="mt-1">{memo.content}</div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default MemoList;
