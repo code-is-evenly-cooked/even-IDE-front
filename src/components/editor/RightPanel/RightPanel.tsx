fa4333370498ae60429be6121755240ea2614778
jdaily00 <kyeonghee_@naver.com>
2025-04-26 15:06:51 +0900

    feat: RightPanel에 메모 패널 추가

--- src/components/editor/RightPanel/RightPanel.tsx
+++ src/components/editor/RightPanel/RightPanel.tsx
@@ -2,12 +2,14 @@
 
 import ChatModal from "./Chat/ChatModal";
 import ChatPanel from "./Chat/ChatPanel";
+import MemoPanel from "./Memo/MemoPanel";
 
 const RightPanel = () => {
 	return (
 		<>
 			<ChatPanel />
 			<ChatModal />
+			<MemoPanel />
 		</>
 	);
 };
