02f3c2a0b159b1ce6475089740e2d3ca0acd0e17
jdaily00 <kyeonghee_@naver.com>
2025-04-26 15:08:10 +0900

    feat: 메모 패널

---
+++ src/components/editor/RightPanel/Memo/MemoPanel.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useMemoStore } from "@/stores/useMemoStore";
+import MemoTitle from "./MemoTitle";
+import MemoList from "./MemoList";
+import MemoInput from "./MemoInput";
+
+const MemoPanel = () => {
+    const { isVisible, viewMode } = useMemoStore();
+
+    if (!isVisible || viewMode !== "panel") return null;
+
+    return (
+        <aside className="w-[320px] h-full bg-gray800 border-none flex flex-col">
+            <MemoTitle />
+            <div className="flex-1 overflow-y-auto px-4 py-2">
+                <MemoList />
+            </div>
+            <MemoInput />
+        </aside>
+    );
+};
+
+export default MemoPanel;
