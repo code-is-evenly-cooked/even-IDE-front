2d9bc9010d4b0dc2abf0f8fa5245a77c7545e932
jdaily00 <kyeonghee_@naver.com>
2025-04-28 03:18:16 +0900

    feat: 인터셉터에서 Authorization 붙이는 코드 삭제

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -4,34 +4,3 @@ export const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     withCredentials: true,
 });
-
-// 쿠키에서 access_token 읽기
-// const getTokenFromCookies = () => {
-//     if (typeof document !== "undefined") {
-//         const cookies = document.cookie.split("; ");
-//         const accessTokenCookie = cookies.find((cookie) =>
-//             cookie.startsWith("access_token=")
-//         );
-//         return accessTokenCookie?.split("=")[1];
-//     }
-//     return null;
-// };
-//
-// export const axiosInstance = axios.create({
-//     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-//     withCredentials: true, // 쿠키도 자동 전송
-// });
-//
-// // 요청 직전 인터셉터에서 Authorization 헤더 추가
-// axiosInstance.interceptors.request.use(
-//     (config) => {
-//         const token = getTokenFromCookies();
-//         if (token) {
-//             config.headers.Authorization = `Bearer ${token}`;
-//         }
-//         return config;
-//     },
-//     (error) => {
-//         return Promise.reject(error);
-//     }
-// );
