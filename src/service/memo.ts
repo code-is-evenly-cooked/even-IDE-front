db22c1779d5dfed4b6ab443f5f8cb0cae8e2f14f
jdaily00 <kyeonghee_@naver.com>
2025-04-28 03:19:41 +0900

    feat: getAuthCookie()로 accessToken 직접 꺼내서 axios 요청 헤더에 넣기, api 경로 수정

--- src/service/memo.ts
+++ src/service/memo.ts
@@ -1,16 +1,42 @@
 import { axiosInstance } from "@/lib/axiosInstance";
+import { getAuthCookie } from "@/lib/cookie";
 
-export const createMemo = async ({projectId, fileId, memo}: { projectId: number, fileId: string, memo: string }) => {
-    const res = await axiosInstance.post(`/projects/${projectId}/files/${fileId}/memos`, { memo });
+export const createMemo = async ({ projectId, fileId, memo }: { projectId: number, fileId: string, memo: string }) => {
+    const { accessToken } = getAuthCookie();
+    const res = await axiosInstance.post(
+        `/projects/${projectId}/file/${fileId}/memos`,
+        { memo },
+        {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        }
+    );
     return res.data;
 };
 
 export const fetchMemos = async (projectId: number, fileId: string) => {
-    const res = await axiosInstance.get(`/projects/${projectId}/files/${fileId}/memos`);
+    const { accessToken } = getAuthCookie();
+    const res = await axiosInstance.get(
+        `/projects/${projectId}/file/${fileId}/memos`,
+        {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        }
+    );
     return res.data;
 };
 
 export const deleteMemoApi = async (projectId: number, fileId: string, memoId: number) => {
-    const res = await axiosInstance.delete(`/projects/${projectId}/files/${fileId}/memos/${memoId}`);
+    const { accessToken } = getAuthCookie();
+    const res = await axiosInstance.delete(
+        `/projects/${projectId}/file/${fileId}/memos/${memoId}`,
+        {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        }
+    );
     return res.data;
 };
