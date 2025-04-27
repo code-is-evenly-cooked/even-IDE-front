ddde0fcf9843a97a937bd86a45b52df045deb1e6
seongmin <min@seongminui-MacBookPro.local>
2025-04-27 14:35:08 +0900

    feat: header에서 토큰 만료 체크 후 만료되었을 경우 로그아웃처리

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import RunButton from "@/components/editor/RunButton";
 import { useAuthStore } from "@/stores/useAuthStore";
 import { removeAuthCookie } from "@/lib/cookie";
@@ -12,54 +12,62 @@ import { clearMessages } from "@/lib/indexedDB";
 import { useAIChatStore } from "@/stores/useAIChatStore";
 import type { Terminal as XtermType } from "xterm";
 import { MenuIcon } from "@/components/common/Icons";
+import { isTokenExpired } from "@/lib/token";
 
 type HeaderProps = {
-  terminalRef: React.RefObject<XtermType>;
+	terminalRef: React.RefObject<XtermType>;
 };
 
 const Header = ({ terminalRef }: HeaderProps) => {
-  const { isLoggedIn, clearAuth } = useAuthStore();
+	const { isLoggedIn, accessToken, clearAuth } = useAuthStore();
 
-  const handleLogout = async () => {
-    removeAuthCookie();
-    clearAuth();
-    useChatStore.getState().resetChatUser();
-    await clearMessages();
-    useAIChatStore.getState().clearMessages();
+	useEffect(() => {
+		if (accessToken && isTokenExpired(accessToken)) {
+			console.log("accessToken 만료");
+			handleLogout();
+		}
+	}, [accessToken, clearAuth]);
 
-	window.location.reload();
-  };
+	const handleLogout = async () => {
+		removeAuthCookie();
+		clearAuth();
+		useChatStore.getState().resetChatUser();
+		await clearMessages();
+		useAIChatStore.getState().clearMessages();
 
-  return (
-    <header className="h-[3rem] flex justify-between items-center p-4">
-      <div className="flex items-center gap-4">
-        <button className="text-xl" aria-label="메뉴 열기">
-          <MenuIcon />
-        </button>
+		window.location.reload();
+	};
 
-        {/* 언어 선택 드롭 박스 */}
-        <LanguageDropdown />
+	return (
+		<header className="h-[3rem] flex justify-between items-center p-4">
+			<div className="flex items-center gap-4">
+				<button className="text-xl" aria-label="메뉴 열기">
+					<MenuIcon />
+				</button>
 
-        {/* 실행 버튼 */}
-        <RunButton terminalRef={terminalRef} />
-        <HeaderActions />
-      </div>
-      <div className="flex">
-        {isLoggedIn ? (
-          <div className="flex items-center gap-2">
-            <span className="gray-200">반갑습니다.</span>
-            <button onClick={handleLogout} className="text-link text-sm">
-              로그아웃
-            </button>
-          </div>
-        ) : (
-          <Link href="/login">
-            <span className="text-link">로그인</span>
-          </Link>
-        )}
-      </div>
-    </header>
-  );
+				{/* 언어 선택 드롭 박스 */}
+				<LanguageDropdown />
+
+				{/* 실행 버튼 */}
+				<RunButton terminalRef={terminalRef} />
+				<HeaderActions />
+			</div>
+			<div className="flex">
+				{isLoggedIn ? (
+					<div className="flex items-center gap-2">
+						<span className="gray-200">반갑습니다.</span>
+						<button onClick={handleLogout} className="text-link text-sm">
+							로그아웃
+						</button>
+					</div>
+				) : (
+					<Link href="/login">
+						<span className="text-link">로그인</span>
+					</Link>
+				)}
+			</div>
+		</header>
+	);
 };
 
 export default Header;
