dfb411cfca08819848ea319cb1a2bb4755512658
jdaily00 <kyeonghee_@naver.com>
2025-04-28 06:03:58 +0900

    feat: 헤더 추가

--- src/service/memo.ts
+++ src/service/memo.ts
@@ -1,11 +1,11 @@
-import { axiosInstance } from "@/lib/axiosInstance";
-import { getAuthCookie } from "@/lib/cookie";
+import {axiosInstance} from "@/lib/axiosInstance";
+import {getAuthCookie} from "@/lib/cookie";
 
-export const createMemo = async ({ projectId, fileId, memo }: { projectId: number, fileId: string, memo: string }) => {
-    const { accessToken } = getAuthCookie();
+export const createMemo = async ({projectId, fileId, memo}: { projectId: number, fileId: string, memo: string }) => {
+    const {accessToken} = getAuthCookie();
     const res = await axiosInstance.post(
         `/projects/${projectId}/file/${fileId}/memos`,
-        { memo },
+        {memo},
         {
             headers: {
                 Authorization: `Bearer ${accessToken}`,
@@ -16,7 +16,17 @@ export const createMemo = async ({ projectId, fileId, memo }: { projectId: numbe
 };
 
 export const fetchMemos = async (projectId: number, fileId: string) => {
-    const res = await axiosInstance.get(`/projects/${projectId}/files/${fileId}/memos`);
+    const {accessToken} = getAuthCookie();
+
+    const res = await axiosInstance.get(
+        `/projects/${projectId}/file/${fileId}/memos`,
+        {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        }
+    );
+
     const memos = res.data;
 
     return memos.map((memo: any) => ({
@@ -33,7 +43,7 @@ export const fetchMemos = async (projectId: number, fileId: string) => {
 };
 
 export const deleteMemoApi = async (projectId: number, fileId: string, memoId: number) => {
-    const { accessToken } = getAuthCookie();
+    const {accessToken} = getAuthCookie();
     const res = await axiosInstance.delete(
         `/projects/${projectId}/file/${fileId}/memos/${memoId}`,
         {
@@ -46,10 +56,10 @@ export const deleteMemoApi = async (projectId: number, fileId: string, memoId: n
 };
 
 export const updateMemoApi = async (projectId: number, fileId: number, memoId: number, memo: string) => {
-    const { accessToken } = getAuthCookie();
+    const {accessToken} = getAuthCookie();
     const res = await axiosInstance.patch(
         `/projects/${projectId}/file/${fileId}/memos/${memoId}`,
-        { memo },
+        {memo},
         {
             headers: {
                 Authorization: `Bearer ${accessToken}`,
